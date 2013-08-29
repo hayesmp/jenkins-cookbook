119e9b80690021047934b6af0149a502f272fe19
Michael Hayes <tasslehawf@mac.com>
2013-08-29 15:25:18 -0500

    recipe command

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.8.0"
 
+recipe "jenkins::server",  "run server recipe."
+
 depends "java"
 depends "runit", ">= 1.0.0"
 depends "apt"
@@ -13,5 +15,3 @@ depends "apt"
 depends "apache2"
 depends "nginx"
 depends "iptables"
-
-recipe "jenkins::server"
